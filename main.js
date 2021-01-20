379458e811e3e0f86f6810e86a344063305e8108
JuanDavila1101 <tatojuan713@hotmail.com>
2021-01-19 20:39:11 -0600

    this is array-methods ex pop push unshift shift

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-console.log("this is arrays")
+// console.log("this is arrays")
+
+const array1 = ['cow', 'dog', 'cat'];
+
+// const string1 = array1.join(', / [');
+
+// console.log(string1);
+// const string2 = string1.split('/');
+
+// .push() & .push()
+// console.log(array1);
+// array1.pop();
+// console.log(array1);
+// array1.push('cat');
+// console.log(array1);
+
+
+// .unshift() & .shift()
+console.log(array1);
+array1.unshift('cat');
+console.log(array1);
+array1.shift();
+console.log(array1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//End
